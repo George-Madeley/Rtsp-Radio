00553817031eed651439d6216deb85b6589614d1
George Madeley <george.madeley@outlook.com>
2025-04-16 11:04:20 +0100

    feat: added CLI argument parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
+#include "argparse/argparse.hpp"
+
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::cout << "Hello, CMake!" << std::endl;
+  argparse::ArgumentParser program("RTSP Radio");
+
+  program.add_argument("-s", "--source").help("File path to the source files").required();
+
+  try
+  {
+    program.parse_args(argc, argv);
+  }
+  catch(const std::exception& err)
+  {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    return 1;
+  }
+
+  auto source = program.get<std::string>("--source");
+  std::cout << "Source: " << source << std::endl;
   return 0;
 }
