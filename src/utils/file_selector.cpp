58879c79d876bee38fbe586b4aaa49b5965afa2f
George Madeley <george.madeley@outlook.com>
2025-04-17 07:04:53 +0100

    refactor: switched from MP3 to WAV

--- src/utils/file_selector.cpp
+++ src/utils/file_selector.cpp
@@ -10,15 +10,15 @@ namespace utils
 {
 std::vector<std::string> FileSelector::get_files(const std::string& directory_path)
 {
-  std::vector<std::string> mp3_files;
+  std::vector<std::string> wav_files;
 
   try
   {
     for(const auto& entry : fs::directory_iterator(directory_path))
     {
-      if(entry.is_regular_file() && entry.path().extension() == ".mp3")
+      if(entry.is_regular_file() && entry.path().extension() == "wav")
       {
-        mp3_files.push_back(entry.path().string());
+        wav_files.push_back(entry.path().string());
       }
     }
   }
@@ -27,7 +27,7 @@ std::vector<std::string> FileSelector::get_files(const std::string& directory_pa
     std::cerr << "Error reading directory: " << e.what() << std::endl;
   }
 
-  return mp3_files;
+  return wav_files;
 }
 
 std::string FileSelector::pick_file(const std::vector<std::string>& files)
@@ -45,6 +45,6 @@ std::string FileSelector::pick_file(const std::vector<std::string>& files)
 bool FileSelector::file_playable(const std::string& file_path)
 {
   std::string extension = file_path.substr(file_path.find_last_of('.') + 1);
-  return (extension == "mp3" || extension == "wav" || extension == "ogg" || extension == "flac");
+  return (extension == "wav");
 }
 } // namespace utils
